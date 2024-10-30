c26c3a6f6cb9f39f8ff077da7c8240a0e3c35b59
Salil-ux <clsalil17@gmail.com>
2024-10-30 15:45:15 +0530

    Update movie_bloc.dart

--- lib/moviebloc/movie_bloc.dart
+++ lib/moviebloc/movie_bloc.dart
@@ -1,30 +1,3 @@
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'api_config.dart';
-// import 'movie_event.dart';
-// import 'movie_state.dart';
-// import 'movie_repository.dart';
-//
-// class MovieBloc extends Bloc<MovieEvent, MovieState> {
-//   final MovieRepository repository;
-//
-//   MovieBloc(this.repository) : super(MovieInitial());
-//
-//   @override
-//   Stream<MovieState> mapEventToState(MovieEvent event) async* {
-//     if (event is FetchMovies) {
-//       yield MovieLoading();
-//       try {
-//         final movies = await repository.fetchMovies(
-//             'trending/movie/week?api_key=f9f4783f569fcd1104aaf0a401c35778'); // Replace with your API key
-//         yield MovieLoaded(movies);
-//       } catch (e) {
-//         yield MovieError(e.toString());
-//       }
-//     }
-//   }
-// }
-// TODO Implement this library.
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'movie_event.dart';
@@ -41,13 +14,6 @@ class MovieBloc extends Bloc<MovieEvent, MovieState> {
         final movies = await movieRepository.fetchMovies(
             'https://api.themoviedb.org/3/movie/top_rated?api_key=f9f4783f569fcd1104aaf0a401c35778');
         emit(MovieLoaded(movies));
-        // try {
-        //   final image = await movieRepository
-        //       .fetchMovies('https://image.tmdb.org/t/p/w500');
-        //   emit(MovieLoaded(image));
-        // } catch (e) {
-        //   emit(MovieError('Failed to fetch movies: $e'));
-        // }
       } catch (e) {
         emit(MovieError('Failed to fetch movies: $e'));
       }
