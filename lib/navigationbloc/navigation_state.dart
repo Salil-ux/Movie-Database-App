99c2191d92ded4ea8fde460206d7c19b6e80b2bb
Salil-ux <clsalil17@gmail.com>
2024-10-30 15:44:46 +0530

    Update navigation_state.dart

--- lib/navigationbloc/navigation_state.dart
+++ lib/navigationbloc/navigation_state.dart
@@ -1,16 +1,3 @@
-// abstract class NavigationState {
-//   final int selectedIndex;
-//   const NavigationState(this.selectedIndex);
-// }
-//
-// class NavigationInitial extends NavigationState {
-//   NavigationInitial() : super(0); // Default to Home
-// }
-//
-// class NavigationChanged extends NavigationState {
-//   NavigationChanged(super.index);
-// }
-
 abstract class NavigationState {
   final int selectedIndex;
 
